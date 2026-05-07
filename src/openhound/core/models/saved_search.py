55728e62cb2eaa729b0409c6cce8ed89d7bc9819
Joey Dreijer <jdreijer@specterops.io>
2026-05-07 16:23:26 +0200

    Add CLI command to generate query bundle (#1)
    * Add CLI command to generate saved searches/query bundle
    * Add test for saved searches bundle generation

--- src/openhound/core/models/saved_search.py
+++ src/openhound/core/models/saved_search.py
@@ -1,9 +1,22 @@
 import json
+import zipfile
+from enum import Enum
+from io import TextIOWrapper
 from pathlib import Path
 from typing import Optional, Union
 
 from pydantic import BaseModel, ConfigDict, field_validator
 from yaml import safe_load
+
+
+class Format(str, Enum):
+    json = "json"
+    yaml = "yaml"
+
+
+class OutputFormat(str, Enum):
+    json = "json"
+    zip = "zip"
 
 
 class SavedSearch(BaseModel):
@@ -14,7 +27,7 @@ class SavedSearch(BaseModel):
     query: str
 
     @classmethod
-    def from_json(cls, file_path: Path) -> "SavedSearch":
+    def from_file(cls, file_path: Path) -> "SavedSearch":
         with open(file_path, "r") as file_object:
             json_object = json.loads(file_object.read())
         return cls(**json_object)
@@ -52,7 +65,49 @@ class SavedSearchExtended(SavedSearch):
         return value if isinstance(value, list) else [value]
 
     @classmethod
-    def from_yaml(cls, file_path: Path) -> "SavedSearchExtended":
+    def from_file(cls, file_path: Path) -> "SavedSearchExtended":
         with open(file_path, "r") as file_object:
             yaml_object = safe_load(file_object.read())
         return cls(**yaml_object)
+
+
+class QueryBundle:
+    def __init__(self, queries: list[SavedSearchExtended | SavedSearch], file_format: Format = Format.json) -> None:
+        self.queries = queries
+        self.file_format = file_format
+
+    @classmethod
+    def from_paths(cls, all_files: list[Path], file_format: Format = Format.json) -> "QueryBundle":
+        model_choices = {
+            'yaml': SavedSearchExtended,
+            'json': SavedSearch,
+        }
+
+        queries = [
+            model_choices[file_format].from_file(cypher_query) for cypher_query in
+            all_files
+        ]
+        return cls(queries, file_format)
+
+    def _to_json(self, output_file: TextIOWrapper) -> None:
+        all_objects = [query.model_dump() for query in self.queries]
+        output_file.write(json.dumps(all_objects, indent=2))
+
+    def _to_zip(self, output_file: TextIOWrapper) -> None:
+        with zipfile.ZipFile(
+                file=output_file.name,
+                mode="w",
+                compression=zipfile.ZIP_DEFLATED,
+                compresslevel=9,
+        ) as archive:
+            for query in self.queries:
+                archive.writestr(
+                    zinfo_or_arcname=f"{query.name}.json",
+                    data=query.model_dump_json().encode(),
+                )
+
+    def save(self, output_file: TextIOWrapper, output_format: OutputFormat = OutputFormat.json) -> None:
+        if output_format == OutputFormat.json:
+            self._to_json(output_file)
+        elif output_format == OutputFormat.zip:
+            self._to_zip(output_file)
