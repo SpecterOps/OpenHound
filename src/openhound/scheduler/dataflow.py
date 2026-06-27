4439d224d8c04f00890a4fa574e3fb49985a758b
Joey Dreijer <jdreijer@specterops.io>
2026-06-27 16:24:19 +0100

    Added additional logging to pipeline execution

--- src/openhound/scheduler/dataflow.py
+++ src/openhound/scheduler/dataflow.py
@@ -1,3 +1,6 @@
+import logging
+from dataclasses import dataclass, field
+from datetime import datetime
 from pathlib import Path
 
 from dlt.common.pipeline import LoadInfo
@@ -6,6 +9,8 @@ from openhound.core.app import OpenHound
 from openhound.core.convert import Method
 from openhound.core.progress import Progress
 
+logger = logging.getLogger(__name__)
+
 
 def extracted_resources(load_info: LoadInfo) -> list[str]:
     all_resources = []
@@ -13,41 +18,65 @@ def extracted_resources(load_info: LoadInfo) -> list[str]:
         for job in package.jobs["completed_jobs"]:
             table = job.job_file_info.table_name
             all_resources.append(table)
-
     return all_resources
 
 
-# TODO: Evaluate using hamilton instead of hardcoding the dataflow. Hamilton should allow easier management for dataflows.
 def collect(extension: OpenHound) -> list[str]:
+    logger.info(
+        f"Starting resource collection phase for {extension.name}",
+        extra={"extension": extension.name},
+    )
     load_info = extension.collector(
         output_path=Path("/tmp/openhound"),  # type: ignore
         resources=[],  # type: ignore
         progress=Progress.log,  # type: ignore
     )
+    logger.info(
+        f"Finished resource collection phase for {extension.name}",
+        extra={"extension": extension.name},
+    )
     return extracted_resources(load_info)
 
 
 def preprocess(extension: OpenHound) -> list[str]:
+    logger.info(
+        f"Starting preprocessing phase for {extension.name}",
+        extra={"extension": extension.name},
+    )
     load_info = extension.preprocessor(
         input_path=Path("/tmp/openhound") / extension.name,
         output_file=Path("lookup.duckdb"),
         progress=Progress.log,  # type: ignore
     )
+    logger.info(
+        f"Finished preprocessing phase for {extension.name}",
+        extra={"extension": extension.name},
+    )
     return extracted_resources(load_info)
 
 
 def convert(extension: OpenHound) -> list[str]:
+    logger.info(
+        f"Starting convertion phase for {extension.name}",
+        extra={"extension": extension.name},
+    )
     load_info = extension.converter(
         input_path=Path("/tmp/openhound") / extension.name,
         method=Method.ingest,  # type: ignore
         progress=Progress.log,  # type: ignore
     )
+    logger.info(
+        f"Finished convertion phase for {extension.name}",
+        extra={"extension": extension.name},
+    )
     return extracted_resources(load_info)
 
 
 def pipeline(extension: OpenHound) -> dict[str, list[str]]:
+    logger.info(
+        f"Starting pipeline for {extension.name}", extra={"extension": extension.name}
+    )
     collect_result = collect(extension)
-
     preprocessor_result = []
     if extension.preprocessor is not None:
         preprocessor_result = preprocess(extension)
